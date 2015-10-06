d8904e5f28cb5c942fa9087cf63e976ac511c88f
vincentchui <vincentchuicalifornia@gmail.com>
2015-10-06 06:41:18 -0700

    code

---
+++ Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Library
+{
+    class Library
+    {
+        ArrayList title = new ArrayList();
+        ArrayList author = new ArrayList();
+        ArrayList lccn = new ArrayList();
+        ArrayList subject = new ArrayList();
+        ArrayList publisher = new ArrayList();
+        ArrayList year = new ArrayList();
+        ArrayList circulation = new ArrayList();
+
+        public void searchBySubject()
+        {
+            string search;
+
+            Console.Write("\nWhat subject to search for? ");
+            search = Console.ReadLine();
+
+            if(subject.Contains(search))
+            {
+
+            }
+        }
+    }
+}
---
+++ LibraryTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class LibraryTester
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
