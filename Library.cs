4d8cef6e78212b65c92ae336c286a9c4e3353671
vincentchui <vincentchuicalifornia@gmail.com>
2015-10-19 12:40:49 -0700

    added more functions

--- Library.cs
+++ Library.cs
@@ -17,6 +17,65 @@ namespace Library
         ArrayList year = new ArrayList();
         ArrayList circulation = new ArrayList();
 
+        public void addBook()
+        {
+            Console.Write("\nWhat is the title? ");
+            title.Add(Console.ReadLine());
+
+            Console.Write("\nWhat is the author? ");
+            author.Add(Console.ReadLine());
+
+            Console.Write("\nWhat is the lccn? ");
+            lccn.Add(Console.ReadLine());
+
+            Console.Write("\nWhat is the subject? ");
+            subject.Add(Console.ReadLine());
+
+            Console.Write("\nWhat is the publisher? ");
+            publisher.Add(Console.ReadLine());
+
+            Console.Write("\nWhat is the year? ");
+            year.Add(Console.ReadLine());
+
+            do
+            {
+                Console.Write("\nIs this in circulation? (Yes/No) ");
+                circulation.Add(Console.ReadLine().ToLower());
+
+                if((Console.ReadLine() != "yes") || (Console.ReadLine() != "no"))
+                {
+                    Console.WriteLine("\nINVALID INPUT");
+                }
+            } while ((Console.ReadLine() != "yes") || (Console.ReadLine() != "no"));
+        }
+
+        public void initArray()
+        {
+            title.Add("C# Programming");
+            author.Add("Microsoft");
+            lccn.Add("0001");
+            subject.Add("Technology");
+            publisher.Add("Microsoft");
+            year.Add("2015");
+            circulation.Add("Yes");
+
+            title.Add("C# Programming 2");
+            author.Add("Microsoft");
+            lccn.Add("0002");
+            subject.Add("Technology");
+            publisher.Add("Microsoft");
+            year.Add("2015");
+            circulation.Add("No");
+
+            title.Add("C# Unity");
+            author.Add("Microsoft");
+            lccn.Add("0001");
+            subject.Add("Technology");
+            publisher.Add("Microsoft");
+            year.Add("2015");
+            circulation.Add("Yes");
+        }
+
         public void searchBySubject()
         {
             string search;
@@ -26,8 +85,37 @@ namespace Library
 
             if(subject.Contains(search))
             {
-
+                Console.WriteLine();
             }
         }
+
+        public void menu()
+        {
+            string choice;
+
+            initArray();
+
+            do
+            {
+                Console.Write("\nSearch by subject" +
+                    "\nAdd Book" +
+                    "\nQuit");
+                choice = Console.ReadLine().ToLower();
+
+                switch (choice)
+                {
+                    case "search by subject":
+                        searchBySubject();
+                        break;
+                    case "quit":
+                        Console.WriteLine("\nGoodbye");
+                        break;
+                    default:
+                        Console.WriteLine("\nINVALID INPUT");
+                        break;
+                }
+            } while (choice != "quit");
+        }
+
     }
 }
